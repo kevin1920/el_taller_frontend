e79c5afd17405bcc872e0c71a1ff176f320aad71
kevin1920 <56529861+kevin1920@users.noreply.github.com>
2020-05-27 21:14:27 -0500

    Se creo el js para el mantenimiento administrador

---
+++ js/mantenimientoAdmin.js
@@ -0,0 +1,147 @@
+let listaMecanicos = []
+
+let listaPlacas = []
+
+let listaMantenimientos = []
+
+let idMecanico = ""
+
+let idMoto = ""
+
+
+let listarMecanicos = () => {
+    let token = localStorage.getItem("token");
+    axios.get("http://localhost:3000/api/v1/mecanicos",{headers:{"token":token}}).then(respuesta => {
+        console.log(respuesta.data)
+        listaMecanicos = respuesta.data
+        let data = ""
+        let lista = document.getElementById("listaMecanicos")
+        for(let i = 0; i < respuesta.data.length; i++){
+            let mecanico = respuesta.data[i]
+            data += "<tr>"
+            data += `<td>${mecanico.documento}</td>`
+            data += `<td>${mecanico.nombre}</td>`
+            data += `<td>${mecanico.apellidos}</td>`
+            data += `<td><button type="button" onclick = "asignarTxtMecanico(${i})" class="btn btn-primary">Asignar</button></td>`
+            data += "</tr>"
+        }
+        lista.innerHTML = data;
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+let listarPlacas = () => {
+    let token = localStorage.getItem("token");
+    let data = ""
+    axios.get("http://localhost:3000/api/v1/placas",{headers:{"token":token}}).then(respuesta => {
+        console.log(respuesta.data)
+        listaPlacas = respuesta.data
+        let lista = document.getElementById("listaPlacas")
+        for(let i = 0; i < respuesta.data.length; i++){
+            let placa = respuesta.data[i]
+            data += "<tr>"
+            data += `<td>${placa.placa}</td>`
+            data += `<td><button type="button" onclick = "asignarTxtPlaca(${i})" class="btn btn-primary">Asignar</button></td>`
+            data += "</tr>"
+        }
+        lista.innerHTML = data;
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+let listarMantenimientos = () => {
+    let token = localStorage.getItem("token");
+    let data = ""
+    axios.get("http://localhost:3000/api/v1/mantenimientoAdmin",{headers:{"token":token}}).then(respuesta => {
+        console.log(respuesta.data)
+        listaMantenimientos = respuesta.data
+        let lista = document.getElementById("listaMantenimientos")
+        for(let i = 0; i < respuesta.data.length; i++){
+            let mantenimiento = respuesta.data[i]
+            data += "<tr>"
+            data += `<td>${mantenimiento.id_mecanico}</td>`
+            data += `<td>${mantenimiento.nombre}</td>`
+            data += `<td>${mantenimiento.apellidos}</td>`
+            data += `<td>${mantenimiento.placa}</td>`
+            data += `<td>${mantenimiento.fecha.slice(0,10)}</td>`
+            data += `<td><button type="button" onclick = "eliminarMantenimiento('${mantenimiento.placa}')" class="btn btn-primary">Eliminar</button></td>`
+            data += "</tr>"
+        }
+        lista.innerHTML = data;
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+let asignarTxtMecanico = index => {
+    let mecanico = listaMecanicos[index]
+    idMecanico = mecanico.documento
+    document.getElementById("txtMecanico").value = `${mecanico.nombre} ${mecanico.apellidos}`
+}
+
+let asignarTxtPlaca = index => {
+    let placa = listaPlacas[index]
+    idMoto = placa.placa
+    document.getElementById("txtPlaca").value = idMoto
+}
+
+let asignarMantenimiento = () => {
+    let date = new Date();
+    let dia = date.getDate()
+    let mes = date.getMonth()+1
+    let año = date.getFullYear()
+    let fecha = año+"-"+mes+"-"+dia;
+    let data = ""
+    let mensaje = document.getElementById("mensajeMantenimiento")
+    if(document.getElementById("txtMecanico").value === "" || document.getElementById("txtPlaca").value === ""){
+        data = `<div class="alert alert-danger" role="alert">
+            Falta el mecanico y/o la placa <a href="#" class="alert-link"></a>
+            </div>`
+            mensaje.innerHTML = data
+    }else{
+        let info = {idMecanico:idMecanico,placa:idMoto,fecha:fecha}
+        let token = localStorage.getItem("token");
+        axios.post("http://localhost:3000/api/v1/mantenimientos",info,{headers:{"token":token}}).then(respuesta => {
+            axios.put(`http://localhost:3000/api/v1/estado/${idMoto}`,{estado:"en reparacion"},{headers:{"token":token}}).then(respuesta => {
+                data = `<div class="alert alert-success" role="alert">
+                El mantenimiento se creo correctamente  <a href="#" class="alert-link"></a>
+                </div>`
+                mensaje.innerHTML = data
+                document.getElementById("txtMecanico").value = ""
+                document.getElementById("txtPlaca").value = ""
+                listarMantenimientos();
+                listarPlacas();
+            }).catch(error => {
+                console.log(error)
+            })
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+}
+
+let eliminarMantenimiento = placa => {
+    let token = localStorage.getItem("token");
+    let data = ""
+    let mensaje = document.getElementById("mensajeMantenimiento")
+    axios.delete(`http://localhost:3000/api/v1/mantenimientos/${placa}`,{headers:{"token":token}}).then(respuesta => {
+        axios.put(`http://localhost:3000/api/v1/estado/${placa}`,{estado:"mala"},{headers:{"token":token}}).then(respuesta => {
+                data = `<div class="alert alert-success" role="alert">
+                El mantenimiento se Elimino correctamente  <a href="#" class="alert-link"></a>
+                </div>`
+                mensaje.innerHTML = data
+                listarMantenimientos();
+                listarPlacas();
+        }).catch(error => {
+             console.log(error)
+        })
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+listarPlacas();
+listarMecanicos();
+listarMantenimientos();
--- js/usuarios.js
+++ js/usuarios.js
@@ -77,12 +77,12 @@ let listarUsuarios = () => {
 let actualizarUsuario = () => {
     let mensaje = document.getElementById("mensajeUsuarios")
     let data = ""
-    document.getElementById("cmbTipoDocumento").value 
-    document.getElementById("txtNombre").value
-    document.getElementById("txtApellidos").value 
-    document.getElementById("txtCelular").value
-    document.getElementById("txtCorreo").value 
-    document.getElementById("cmbRoles").value 
+    let tipoDocumento = document.getElementById("cmbTipoDocumento").value 
+    let nombre = document.getElementById("txtNombre").value
+    let apellidos = document.getElementById("txtApellidos").value 
+    let celular = document.getElementById("txtCelular").value
+    let correo = document.getElementById("txtCorreo").value 
+    let rol = document.getElementById("cmbRoles").value 
     let info = {tipoDocumento:tipoDocumento,nombre:nombre,apellidos:apellidos,celular:celular,correo:correo,rol:rol}
     let token = localStorage.getItem("token");
     axios.put(`http://localhost:3000/api/v1/usuarios/${usuarioActualizar}`,info,{headers:{"token":token}}).then(respuesta => {
@@ -91,8 +91,8 @@ let actualizarUsuario = () => {
         El usuario se actualizo correctamente <a href="#" class="alert-link"></a>
         </div>`
         mensaje.innerHTML = data
-        document.getElementById("txtDocumento").style.display = "inline"
-        document.getElementById("txtContraseña").style.display = "inline"
+        document.getElementById("documento").style.display = "inline"
+        document.getElementById("contraseña").style.display = "inline"
         document.getElementById("btnCrearUsuario").style.display = "inline"
         document.getElementById("btnActualizarUsuario").style.display = "none"
         listarUsuarios()
@@ -105,6 +105,7 @@ let actualizarUsuario = () => {
 let eliminarUsuario = documento => {
     let mensaje = document.getElementById("mensajeUsuarios")
     let data = ""
+    let token = localStorage.getItem("token");
     axios.delete(`http://localhost:3000/api/v1/usuarios/${documento}`,{headers:{"token":token}}).then(respuesta => {
         data = `<div class="alert alert-success" role="alert">
         El usuario se elimino correctamente <a href="#" class="alert-link"></a>
@@ -119,14 +120,35 @@ let eliminarUsuario = documento => {
 let cargarDatosUsuario = index => {
     let usuario = listaUsuarios[index]
     usuarioActualizar = usuario.documento
-    document.getElementById("cmbTipoDocumento").value =  usuario.tipo_documento
-    document.getElementById("txtDocumento").style.display = "none"
+    let rolValue =""
+    let tipoDocumentoValue = ""
+    switch (usuario.tipo_documento) {
+        case 'CC':
+            tipoDocumentoValue = 1
+            break;
+        case 'CE':
+            tipoDocumentoValue = 2
+            break;
+        case 'NIT':
+            tipoDocumentoValue = 3
+            break;
+        case 'Pasaporte':
+            tipoDocumentoValue = 4
+            break;             
+    }
+    if(usuario.rol === 'Mécanico'){
+        rolValue = 1
+    }else{
+        rolValue = 2
+    }
+    document.getElementById("cmbTipoDocumento").value = tipoDocumentoValue
+    document.getElementById("documento").style.display = "none"
     document.getElementById("txtNombre").value =  usuario.nombre
     document.getElementById("txtApellidos").value =  usuario.apellidos
     document.getElementById("txtCelular").value = usuario.celular
     document.getElementById("txtCorreo").value =  usuario.correo
-    document.getElementById("cmbRoles").value =  usuario.rol
-    document.getElementById("txtContraseña").style.display = "none"
+    document.getElementById("cmbRoles").value = rolValue
+    document.getElementById("contraseña").style.display = "none"
     document.getElementById("btnCrearUsuario").style.display = "none"
     document.getElementById("btnActualizarUsuario").style.display = "inline"
 }
@@ -141,3 +163,5 @@ let limpiarCamposUsuarios = () => {
     document.getElementById("cmbRoles").value = ""
     document.getElementById("txtContraseña").value = ""
 }
+
+listarUsuarios();
